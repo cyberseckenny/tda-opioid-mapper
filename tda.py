3bebf957bbe7630a474ce9e371cea3be89ea8300
cyberseckenny <kennycs@protonmail.com>
2024-04-28 22:28:27 -0400

    Got some shape for the data

--- tda.py
+++ tda.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 import sklearn
 
-x, y, z, j, k = [], [], [], [], []
+d = {}
+type_1 = 'Synthetic opioids, excl. methadone (T40.4)'
+type_2 = 'Heroin (T40.1)'
+type_3 = 'Number of Drug Overdose Deaths'
 
 with open('VSRR_Provisional_Drug_Overdose_Death_Counts.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -19,45 +22,43 @@ with open('VSRR_Provisional_Drug_Overdose_Death_Counts.csv') as csv_file:
         year = row[1]
         indicator = row[4]
         state = row[8]
-        month = row[2]
-
+        month = row[2]  
+        
         if row[5] == '':
             continue
 
         deaths = int(float(row[5].replace(',', '')))
-        
-        # what actually matters
 
-        if state != "United States":
-            if indicator == 'Synthetic opioids, excl. methadone (T40.4)':
-                x.append(deaths)
-            elif indicator == 'Heroin (T40.1)':
-                y.append(deaths)
-            elif indicator == 'Number of Drug Overdose Deaths':
-                z.append(deaths)
+        if state == 'United States':
+            continue
 
-             
-        # elif indicator == 'Methadone (T40.3)':
-        #      j.append(deaths)
+        if indicator in [type_1, type_2, type_3]:
+            key = (state, year, month)
+            if key not in d:
+                d[key] = {}
+            d[key][indicator] = deaths
 
-data = np.array(list(zip(x, y, z)))
-print(len(x), len(y), len(z))
+result = []
+for state_year_month, values in d.items():
+    synthetic_opioids = values.get(type_1, None) 
+    heroin = values.get(type_2, None) 
+    number = values.get(type_3, None)
+    if all(values.values()) and synthetic_opioids is not None and heroin is not None and number is not None:  # Check if all values are not empty
+        result.append([values[type_1], 
+                        values[type_2], values[type_3]])
+
+data = np.array(result)
 # data, labels = datasets.make_circles(n_samples=5000, noise=0.1, factor=0.3)
 
-# mapper = km.KeplerMapper(verbose=2)
-# lens = mapper.fit_transform(data)
-# graph = mapper.map(
-#     lens,
-#     data,
-#     clusterer=sklearn.cluster.DBSCAN(eps=100000, min_samples=1),
-#     cover=km.Cover(30, 0.2),
-# )
-# mapper.visualize(graph, path_html="tda.html")
-
-min_length = min(len(x), len(y), len(z))  
-x = x[:min_length]
-y = y[:min_length]
-z = z[:min_length]
+mapper = km.KeplerMapper(verbose=2)
+lens = mapper.fit_transform(data)
+graph = mapper.map(
+    lens,
+    data,
+    clusterer=sklearn.cluster.DBSCAN(eps=200, min_samples=6),
+    cover=km.Cover(15, 0.2),
+)
+mapper.visualize(graph, path_html="tda.html", title='Opioid Deaths')
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
