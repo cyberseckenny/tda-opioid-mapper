b2de1c317809c9a4b8904c5e2d6956824255121b
cyberseckenny <kennycs@protonmail.com>
2024-04-28 21:30:01 -0400

    Picked data I like

--- tda.py
+++ tda.py
@@ -28,31 +28,31 @@ with open('VSRR_Provisional_Drug_Overdose_Death_Counts.csv') as csv_file:
         
         # what actually matters
 
-        if state != "New York City":
-            continue
+        if state != "United States":
+            if indicator == 'Synthetic opioids, excl. methadone (T40.4)':
+                x.append(deaths)
+            elif indicator == 'Heroin (T40.1)':
+                y.append(deaths)
+            elif indicator == 'Number of Drug Overdose Deaths':
+                z.append(deaths)
 
-        if indicator == 'Synthetic opioids, excl. methadone (T40.4)':
-            x.append(deaths)
-        elif indicator == 'Heroin (T40.1)':
-            y.append(deaths)
-        elif indicator == 'Cocaine (T40.5)':
-             z.append(deaths)
+             
         # elif indicator == 'Methadone (T40.3)':
         #      j.append(deaths)
 
 data = np.array(list(zip(x, y, z)))
-print(data)
+print(len(x), len(y), len(z))
 # data, labels = datasets.make_circles(n_samples=5000, noise=0.1, factor=0.3)
 
-mapper = km.KeplerMapper(verbose=2)
-lens = mapper.fit_transform(data)
-graph = mapper.map(
-    lens,
-    data,
-    clusterer=sklearn.cluster.DBSCAN(eps=100000, min_samples=1),
-    cover=km.Cover(30, 0.2),
-)
-mapper.visualize(graph, path_html="tda.html")
+# mapper = km.KeplerMapper(verbose=2)
+# lens = mapper.fit_transform(data)
+# graph = mapper.map(
+#     lens,
+#     data,
+#     clusterer=sklearn.cluster.DBSCAN(eps=100000, min_samples=1),
+#     cover=km.Cover(30, 0.2),
+# )
+# mapper.visualize(graph, path_html="tda.html")
 
 min_length = min(len(x), len(y), len(z))  
 x = x[:min_length]
@@ -61,6 +61,10 @@ z = z[:min_length]
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
+
+ax.set_xlabel('Synthetic opioids')
+ax.set_ylabel('Heroin')
+ax.set_zlabel('Overall Drug Overdose Deaths')
 
 ax.scatter(data[: ,0], data[: ,1], data[: ,2])
 plt.show()
